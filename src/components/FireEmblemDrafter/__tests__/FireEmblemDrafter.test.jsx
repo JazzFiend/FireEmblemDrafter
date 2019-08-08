3199c948971eb7e470e2c41c19206fcdd22c6c0a
Phil Deitz <philipd@spotify.com>
2019-08-08 16:10:44 -0400

    Fix lint command and linter errors

--- src/components/FireEmblemDrafter/__tests__/FireEmblemDrafter.test.jsx
+++ src/components/FireEmblemDrafter/__tests__/FireEmblemDrafter.test.jsx
@@ -4,31 +4,31 @@ import {
   fireEvent,
   cleanup, } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { FireEmblemDrafter } from '../FireEmblemDrafter.js';
+import { FireEmblemDrafter } from '../FireEmblemDrafter';
 
 afterEach(cleanup);
 
 test('renders and matches snapshot', () => {
-  const { asFragment } = render(<FireEmblemDrafter roster={ ['x', 'y', 'z'] } teamSize={ 2 } isRandom={ false }/>);
+  const { asFragment } = render(<FireEmblemDrafter roster={['x', 'y', 'z']} teamSize={2} isRandom={false} />);
   expect(asFragment()).toMatchSnapshot();
 });
 
 test('draft should start when button is clicked', () => {
-  const { getByTestId } = render(<FireEmblemDrafter roster={ ['1', '2'] } teamSize={ 1 } isRandom={ false }/>);
+  const { getByTestId } = render(<FireEmblemDrafter roster={['1', '2']} teamSize={1} isRandom={false} />);
   fireEvent.click(getByTestId('start-draft-button'));
   expect(getByTestId('start-draft-button')).toHaveTextContent('Restart Draft!');
   expect(getByTestId('pick')).toBeVisible();
 })
 
 test('should add to team when pick is selected', () => {
-  const { getByTestId, getAllByTestId } = render(<FireEmblemDrafter roster={ ['a', 'b', 'c'] } teamSize={ 1 } isRandom={ false }/>);
+  const { getByTestId, getAllByTestId } = render(<FireEmblemDrafter roster={['a', 'b', 'c']} teamSize={1} isRandom={false} />);
   fireEvent.click(getByTestId('start-draft-button'));
   fireEvent.click(getAllByTestId('pick-button')[0]);
   expect(getByTestId('team-list')).toHaveTextContent('b');
 });
 
 test('should complete draft when draft is over', () => {
-  const { getByTestId, getAllByTestId } = render(<FireEmblemDrafter roster={ ['1', '2', '3'] } teamSize={ 2 } isRandom={ false }/>);
+  const { getByTestId, getAllByTestId } = render(<FireEmblemDrafter roster={['1', '2', '3']} teamSize={2} isRandom={false} />);
   fireEvent.click(getByTestId('start-draft-button'));
   fireEvent.click(getAllByTestId('pick-button')[0]);
   fireEvent.click(getAllByTestId('pick-button')[0]);
--- src/components/__tests__/Pick.test.jsx
+++ src/components/__tests__/Pick.test.jsx
@@ -1,22 +1,23 @@
+import _ from 'lodash';
 import React from 'react';
 import {
   render,
   fireEvent,
   cleanup, } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { Pick } from '../Pick';
-import _ from 'lodash';
+
+import Pick from '../Pick';
 
 afterEach(cleanup);
 
 test('renders and matches snapshot', () => {
-  const { asFragment } = render(<Pick pick={ ['a', 'b', 'c'] } onClick={() => {}} />);
+  const { asFragment } = render(<Pick pick={['a', 'b', 'c']} onClick={() => {}} />);
   expect(asFragment()).toMatchSnapshot();
 });
 
 test('contains appropriate buttons', () => {
   const testPick =  ['x', 'y', 'z'];
-  const { getAllByTestId } = render(<Pick pick={ testPick } onClick={() => {}} />);
+  const { getAllByTestId } = render(<Pick pick={testPick} onClick={() => {}} />);
   let pickButtons = getAllByTestId('pick-button');
 
   expect(pickButtons.length).toBe(3);
@@ -27,7 +28,7 @@ test('contains appropriate buttons', () => {
 
 test('click runs passed in function', () => {
   const onClick = jest.fn();
-  const { getByTestId } = render(<Pick pick={ ['x'] } onClick={(index) => onClick(index)} />);
+  const { getByTestId } = render(<Pick pick={['x']} onClick={(index) => onClick(index)} />);
 
   fireEvent.click(getByTestId('pick-button'));
   expect(onClick).toHaveBeenCalledWith(0);
